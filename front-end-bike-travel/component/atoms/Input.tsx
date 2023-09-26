0576e1d7729fed124e497d2896de63068966d7d2
vincent <vincent@trakmy.fr>
2023-09-26 14:05:12 +0200

    login

---
+++ front-end-bike-travel/component/atoms/Input.tsx
@@ -0,0 +1,10 @@
+import { Input } from '@chakra-ui/react'
+import React, { Component } from 'react'
+
+export default class Login extends Component {
+  render() {
+    return (
+      <Input />
+    )
+  }
+}
