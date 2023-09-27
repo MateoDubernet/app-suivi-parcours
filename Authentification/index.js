295c525f8d1fa2d82ad3b0327b764be29fbd128a
Matéo Dubernet-Gliksten <mateo.dubernet@cogis.com>
2023-09-27 14:25:41 +0200

    change db connect info

--- Authentification/index.js
+++ Authentification/index.js
@@ -12,7 +12,6 @@ const connection = mysql.createConnection({
   host: process.env.DB_HOST || 'localhost',
   user: process.env.DB_USER || 'root',
   password: process.env.DB_PASSWORD || 'root',
-  database: 'Bike_Travel_User',
   port: process.env.DB_PORT || 3306
 });
 
