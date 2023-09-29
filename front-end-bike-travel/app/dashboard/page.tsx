6615df994cc46de5e982aca420732cf82d83cf2d
vincent <vincent@trakmy.fr>
2023-09-29 15:47:56 +0200

    page profiel

--- front-end-bike-travel/app/dashboard/page.tsx
+++ front-end-bike-travel/app/dashboard/page.tsx
@@ -2,7 +2,7 @@
 import Map from '@/component/atoms/windows'
 import { Button, Heading } from '@chakra-ui/react'
 import { useRouter } from 'next/navigation';
-import React, { useEffect, useState } from 'react'
+import React, {  useState } from 'react'
 import style from '../../app/page.module.css';
 import { protectRoute } from '@/component/atoms/protectRoute';
 import Cookies from 'js-cookie';
@@ -15,14 +15,24 @@ export default protectRoute(function Dashboard() {
     router.push(`${route}`);
   };
 
-  const [user , setUser] = useState(0)
-
+  const handleProfil = (route: string) => {
+    router.push(`${route}`)
+  }
   return (
     <>
       <nav className={style.navbar}>
         <div className={style.title}>
           <Heading>Dashboard</Heading>
         </div>
+        <Button
+           className={style.disconnect}
+          variant={"unstyled"}
+          color={"white"}
+          backgroundColor={"#ec7402"}
+          onClick={() => handleProfil("/profile")}
+          rounded={"full"}>
+          Mon profil
+        </Button>
         <Button
           className={style.disconnect}
           variant={"unstyled"}
